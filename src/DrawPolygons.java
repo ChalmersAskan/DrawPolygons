abc15cada276f6dd5c7425858736ed52c72b2223
ChalmersAskan <143622583+ChalmersAskan@users.noreply.github.com>
2023-11-06 14:56:39 +0100

    försök på uppgift 1

--- src/DrawPolygons.java
+++ src/DrawPolygons.java
@@ -2,11 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 
+
 public class DrawPolygons extends Component{
-    private ArrayList<String> polygonNames;
-    private ArrayList<Point>  centerPoints;
+    //private ArrayList<String> polygonNames;
+    //private ArrayList<Point>  centerPoints;
+    private ArrayList<Polygon> polygons;
 
     public DrawPolygons(){
+        polygons = new ArrayList<>(10);
+        polygons.add(new Polygon("square", new Point(100,100)));
+        polygons.add(new Polygon("triangle", new Point(150,150)));
+        polygons.add(new Polygon("rectangle", new Point(100,200)));
+
+
+        /*
         polygonNames = new ArrayList<>(10);
         centerPoints = new ArrayList<>(10);
         polygonNames.add("square");
@@ -15,23 +24,26 @@ public class DrawPolygons extends Component{
         centerPoints.add(new Point(100,100));
         centerPoints.add(new Point(150,150));
         centerPoints.add(new Point(100,200));
+        */
+        
     }//constructor
 
     @Override
     public void paint(Graphics g) {
-        for (int i = 0; i < polygonNames.size(); i++) {
-            String currentPolygon = polygonNames.get(i);
-            Point currentCenter = centerPoints.get(i);
-            if (currentPolygon.equals("square"))
+        for (int i = 0; i < polygons.size(); i++) {
+            Polygon currentPolygon = polygons.get(i);
+            String currentShape = currentPolygon.name;
+            Point currentCenter = currentPolygon.centerPoint;
+            if (currentShape.equals("square"))
                 g.drawRect(currentCenter.x -10, currentCenter.y -10, 20, 20);
-            else if (currentPolygon.equals("triangle")) {
+            else if (currentShape.equals("triangle")) {
                 g.drawLine(currentCenter.x, currentCenter.y-10, currentCenter.x-10,
                         currentCenter.y+10);
                 g.drawLine(currentCenter.x-10, currentCenter.y+10,
                         currentCenter.x+10, currentCenter.y+10);
                 g.drawLine(currentCenter.x+10, currentCenter.y+10, currentCenter.x,
                         currentCenter.y-10);
-            } else if (currentPolygon.equals("rectangle"))
+            } else if (currentShape.equals("rectangle"))
                 g.drawRect(currentCenter.x -20, currentCenter.y -10, 40, 20);
         }
     }//paint
@@ -45,3 +57,14 @@ public class DrawPolygons extends Component{
         frame.setVisible(true);
     }//main
 }//DrawPolygons
+
+
+class Polygon {
+    String name;
+    Point centerPoint;
+
+    public Polygon(String newName, Point point) {
+        name = newName;
+        centerPoint = point;
+    }
+}
